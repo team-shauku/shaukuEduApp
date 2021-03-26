83311ef1404c2bc41ad520d0812eee953a2c74b6
larryokubasu5460 <bulumalarry3@gmail.com>
2021-03-27 00:37:31 +0300

    forms set up

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+
+Route::get('/create_course', [App\Http\Controllers\CourseController::class, 'index']);
+
