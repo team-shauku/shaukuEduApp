b60f3d45f38068f8f8f417790acf684255ac75e0
Oluyosola <afolabioluyosola@gmail.com>
2021-04-04 20:17:19 +0100

    redirecting to homepage

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -9,12 +9,12 @@ class Role {
 
   public function handle($request, Closure $next, String $role) {
     if (!Auth::check()) // This isnt necessary, it should be part of your 'auth' middleware
-      return redirect('/home');
+      return redirect('/');
 
     $user = Auth::user();
     if($user->role == $role)
       return $next($request);
 
-    return redirect('/home');
+    return redirect('/');
   }
 }
